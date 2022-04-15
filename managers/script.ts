d96af992ff4ab2c006fca500d74a54446962e06a
Shawn Tabai <stabai@gmail.com>
2022-04-15 01:17:25 -0700

    Implement install scripts

--- managers/script.ts
+++ managers/script.ts
@@ -1,8 +1,15 @@
-import { PackageManager, PackageManagerStatus, SoftwarePackage } from '../api/package_types.ts';
+import { download } from 'https://deno.land/x/download@v1.0.1/mod.ts';
 
-export interface InstallScript extends SoftwarePackage<'script'> {
+import { PackageManager, PackageManagerStatus, SoftwarePackage } from '../api/package_types.ts';
+import { run } from '../shell/run.ts';
+
+export interface RemoteInstallScript extends SoftwarePackage<'script'> {
   scriptUrl: string;
 }
+export interface LocalInstallScript extends SoftwarePackage<'script'> {
+  scriptPath: string;
+}
+export type InstallScript = RemoteInstallScript | LocalInstallScript;
 
 export class ScriptInstaller extends PackageManager<'script', InstallScript> {
   override readonly name = 'script';
@@ -11,7 +18,14 @@ export class ScriptInstaller extends PackageManager<'script', InstallScript> {
     return Promise.resolve('ready');
   }
 
-  installPackage(pkg: InstallScript): Promise<void> {
-    throw new Error('Function not implemented.');
+  async installPackage(pkg: InstallScript): Promise<void> {
+    let localFilePath: string;
+    if ('scriptUrl' in pkg) {
+      const downloaded = await download(pkg.scriptUrl);
+      localFilePath = downloaded.fullPath;
+    } else {
+      localFilePath = pkg.scriptPath;
+    }
+    await run([localFilePath]);
   }
 }
